7e9a11e3a4e5eb87efbff1fba1191df1cdb3e720
coolio982 <wendy.gao@live.co.uk>
2023-05-23 09:38:35 +0100

    first commit

---
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+
+
+class KeyPointClassifier(object):
+    def __init__(
+        self,
+        model_path='model/keypoint_classifier/keypoint_classifier.tflite',
+        num_threads=1,
+    ):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path,
+                                               num_threads=num_threads)
+
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+    def __call__(
+        self,
+        landmark_list,
+    ):
+        input_data = np.array([landmark_list], dtype=np.float32)
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+        self.interpreter.invoke()
+        result = self.interpreter.get_tensor(self.output_details[0]['index'])
+        result_index = np.argmax(np.squeeze(result))
+        return result_index
